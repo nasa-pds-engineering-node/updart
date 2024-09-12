ef2677e8e93f7dc8f6986ad0fbc6d43f487d68a6
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2024-09-11 22:20:29 -0400

    add of_collection method, upgrade pds.api-client dependency

--- src/pds/updart/client.py
+++ src/pds/updart/client.py
@@ -1,11 +1,14 @@
 """PDS Registry Client lreated classes."""
 from __future__ import print_function
 
+import logging
 from datetime import datetime
 
 from pds.api_client import ApiClient
 from pds.api_client import Configuration
 from pds.api_client.api.all_products_api import AllProductsApi
+
+logger = logging.getLogger(__name__)
 
 DEFAULT_API_BASE_URL = "https://pds.nasa.gov/api/search/1"
 
@@ -26,6 +29,9 @@ class PDSRegistryClient:
 class Products:
     """Use to access any class of planetary products."""
 
+    SORT_PROPERTY = "ops:Harvest_Info.ops:harvest_date_time"
+    PAGE_SIZE = 100
+
     def __init__(self, client: PDSRegistryClient):
         """Use to access any class of planetary products.
 
@@ -33,7 +39,9 @@ class Products:
         """
         self._products = AllProductsApi(client.api_client)
         self._q_string = ""
+        self._latest_harvest_time = None
         self._crt_page_iterator = None
+        self._counter_in_page = None
 
     def __add_clause(self, clause):
         clause = f"({clause})"
@@ -76,15 +84,48 @@ class Products:
         self.__add_clause(clause)
         return self
 
+    def of_collection(self, id: str):
+        """Selects products which belong to a collection.
+
+        :param id: the collection id, e.g. a lidvid
+        :return: a Products instance with after filter applied
+        """
+        clause = f'ops:Provenance.ops:parent_collection_identifier eq "{id}"'
+        self.__add_clause(clause)
+        return self
+
+    def _init_new_page(self):
+        logger.info("get new page from API")
+
+        # if not first page, this was the last page is number of products is less than page size.
+        if self._counter_in_page is not None and self._counter_in_page < self.PAGE_SIZE:
+            raise StopIteration("This was the last page")
+
+        kwargs = dict()
+        kwargs["sort"] = [self.SORT_PROPERTY]
+        kwargs["limit"] = self.PAGE_SIZE
+
+        if self._latest_harvest_time is not None:
+            kwargs["search_after"] = [self._latest_harvest_time]
+
+        if len(self._q_string) > 0:
+            kwargs["q"] = f"({self._q_string})"
+
+        results = self._products.product_list(**kwargs)
+
+        self._counter_in_page = 0
+        self._crt_page_iterator = iter(results.data)
+
     def __iter__(self):
         """:return: an iterator on the filtered products."""
-        if len(self._q_string) > 0:
-            results = self._products.product_list(q=f"({self._q_string})")
-        else:
-            results = self._products.product_list()
-
-        self._crt_page_iterator = iter(results.data)
+        self._init_new_page()
         return self
+
+    def _get_new_product(self):
+        self._counter_in_page += 1
+        next_product = next(self._crt_page_iterator)
+        self._latest_harvest_time = next_product.properties[self.SORT_PROPERTY][0]
+        return next_product
 
     def __next__(self):
         """Iterate on the full list of filtered results.
@@ -94,7 +135,7 @@ class Products:
         :return: next filtered product
         """
         try:
-            return next(self._crt_page_iterator)
+            return self._get_new_product()
         except StopIteration:
-            # TODO: implement next page retrieval and return first element of next page
-            raise
+            self._init_new_page()
+            return self._get_new_product()
--- src/pds/updart/tests/test_client.py
+++ src/pds/updart/tests/test_client.py
@@ -9,32 +9,60 @@ class ClientTestCase(unittest.TestCase):
         pass
 
     def test_all(self):
+        """
+        Up to 1000 products
+        :return:
+        """
         client = upd.PDSRegistryClient()
         products = upd.Products(client)
+        n = 0
         for p in products:
+            n += 1
             print(p.id)
+            if n > 1000:
+                break
         assert True
 
     def test_has_target(self):
         client = upd.PDSRegistryClient()
         products = upd.Products(client)
         lidvid = "urn:nasa:pds:context:target:asteroid.65803_didymos"
+        n = 0
         for p in products.has_target(lidvid):
+            n += 1
             assert lidvid in p.properties["ref_lid_target"]
+            if n > 1000:
+                break
 
     def test_before(self):
         client = upd.PDSRegistryClient()
         products = upd.Products(client)
         iso8601_date = "2005-07-06T05:50:23Z".replace("Z", "+00:00")
         date_ref = datetime.fromisoformat(iso8601_date)
+        n = 0
         for p in products.before(date_ref):
+            n += 1
             iso8601_date_found = p.start_date_time.replace("Z", "+00:00")
             date_found = datetime.fromisoformat(iso8601_date_found)
             assert date_found <= date_ref
+            if n > 1000:
+                break
 
     def test_after(self):
         # TODO implement
         pass
+
+    def test_products_of_collection(self):
+        client = upd.PDSRegistryClient()
+        products = upd.Products(client)
+        id = "urn:nasa:pds:apollo_pse:data_seed::1.0"
+        n = 0
+        for p in products.of_collection(id):
+            n += 1
+            found_id = p.properties["ops:Provenance.ops:parent_collection_identifier"][0]
+            assert found_id == id
+            if n > 1000:
+                break
 
     def test_before_and_has_target(self):
         client = upd.PDSRegistryClient()
@@ -42,11 +70,15 @@ class ClientTestCase(unittest.TestCase):
         lidvid = "urn:nasa:pds:context:target:comet.9p_tempel_1"
         iso8601_date = "2005-07-06T05:47:25+00:00"
         date_ref = datetime.fromisoformat(iso8601_date)
+        n = 0
         for p in products.has_target(lidvid).before(date_ref):
+            n += 1
             assert lidvid in p.properties["ref_lid_target"]
             iso8601_date_found = p.start_date_time.replace("Z", "+00:00")
             date_found = datetime.fromisoformat(iso8601_date_found)
             assert date_found <= date_ref
+            if n > 1000:
+                break
 
 
 if __name__ == "__main__":
