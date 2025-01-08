3aadf9553d012add3b5d4b1b359edba24f46d09a
Scott Collins <collinss@jpl.nasa.gov>
2025-01-08 13:50:12 -0800

    Initial addition of the orex package
    This package contains an specialized query builder implementation that supports within_range and within_bbox calls for use with Osiris Rex products.

--- src/pds/peppi/__init__.py
+++ src/pds/peppi/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 """PDS peppi."""
 from .client import PDSRegistryClient  # noqa
+from .orex import OrexProducts  # noqa
 from .products import Products  # noqa
-
-# For future consideration:
-#
-# - Other metadata (__docformat__, __copyright__, etc.)
-# - N̶a̶m̶e̶s̶p̶a̶c̶e̶ ̶p̶a̶c̶k̶a̶g̶e̶s̶ we got this
