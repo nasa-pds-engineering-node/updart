de3b912b66024b215420882f27bd3a75137086d2
Scott Collins <collinss@jpl.nasa.gov>
2025-01-23 11:15:59 -0800

    Products docstring cleanup

--- src/pds/peppi/products.py
+++ src/pds/peppi/products.py
@@ -6,8 +6,9 @@ from .query_builder import QueryBuilder
 class Products(QueryBuilder):
     """Use to access any class of planetary products via the PDS Registry API.
 
-    This class is both a :class:`.query_builder.QueryBuilder` which carries methods to subset the products
-    and a :class:`.result_set.ResultSet` which can be iterated on or converted to for example a pandas DataFrame
+    This class is an inheritor of :class:`.query_builder.QueryBuilder`, which
+    carries methods to subset the products, and which can be iterated on or
+    converted to a pandas DataFrame.
     """
 
     def __init__(self, client: PDSRegistryClient):
